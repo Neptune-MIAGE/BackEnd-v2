b84ec203be5990aa1e5221eb28241cb39731d55b
ryad1602 <ryadmessaoudi08@gmail.com>
2024-12-13 19:01:46 +0100

    mise en place des models et de l'architecture

--- moodapp/moods/views.py
+++ moodapp/moods/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render
 
 from django.http import JsonResponse
 from .models import Mood, UserMood
+from django.views.decorators.http import require_http_methods
+
 
 # Vue pour afficher la liste des humeurs
+@require_http_methods(["GET", "POST"])  # Sensitive
 def list_moods(request):
     moods = Mood.objects.all().values("id", "name", "description", "created_at")
     return JsonResponse(list(moods), safe=False)
 
 # Vue pour enregistrer une humeur pour un utilisateur
+@require_http_methods(["GET", "POST"])  # Sensitive
 def add_user_mood(request):
     if request.method == "POST":
         user_id = request.POST.get("user_id")
@@ -18,6 +22,7 @@ def add_user_mood(request):
         return JsonResponse({"message": "Mood added successfully!", "id": user_mood.id})
 
 # Vue pour afficher les humeurs d’un utilisateur
+@require_http_methods(["GET", "POST"])  # Sensitive
 def user_moods(request, user_id):
     user_moods = UserMood.objects.filter(user_id=user_id).values("mood__name", "date")
     return JsonResponse(list(user_moods), safe=False)
