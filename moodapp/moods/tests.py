409b5beb0be5582088e4a196012b2c5d3fec0824
KVNS7 <ksoares@outlook.fr>
2024-12-14 15:33:28 +0100

    Edit test for CI debug

--- moodapp/moods/tests.py
+++ moodapp/moods/tests.py
@@ -3,5 +3,5 @@ from django.test import TestCase
 # Create your tests here.
 class BasicTest(TestCase):
     def test_home_page(self):
-        response = self.client.get("/moods")
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 404)
